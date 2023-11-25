d1e05a4a9dede7c739acc650eb4270436baf79d8
ItsWachira <brianwachira79@gmail.com>
2023-11-25 23:59:39 +0300

    added a sidebar component using react-pro-sidebar

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+
+import Sidebar from "../components/SidebarComponent.js";
+
+export default function Home () {
+  return (
+    <main>
+      <Sidebar />
+    </main>
+  )
+}
+
+
+
+
+
